e607c81a481f1ebb5caefb19cdd73372b10ded84
Tjalling Feenstra <hello@tjalling.net>
2025-09-24 01:05:39 +0200

    fix: database not available causes error

--- src/DynamicPageServiceProvider.php
+++ src/DynamicPageServiceProvider.php
@@ -3,17 +3,21 @@ namespace Tjall\Pagebuilder;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 use Tjall\Pagebuilder\Models\Page;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Schema;
 use Tjall\Pagebuilder\Http\Controllers\DynamicPageController;
 
 class DynamicPageServiceProvider extends ServiceProvider {
     protected const ROUTES_CACHE_KEY = 'pagebuilder_routes';
 
     public function boot(): void {
-        app()->booted(function() {
+        try {
             $this->cacheRoutes();
-        });
+        } catch(\Exception $e) {
+            // Database may nog be available, ignore error
+        }
     }
 
     public function cacheRoutes(): void {
